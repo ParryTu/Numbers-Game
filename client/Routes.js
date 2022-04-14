7d3a3c299f33249ec086a1b147db69e1c733d2c2
Parry <pt987@nyu.edu>
2022-04-14 10:46:30 -0400

    removed unneeded boilerplate code. added numbers api and render

--- client/Routes.js
+++ client/Routes.js
@@ -1,59 +1,75 @@
-import React, {Component, Fragment} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import { Login, Signup } from './components/AuthForm';
-import Home from './components/Home';
-import {me} from './store'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Login, Signup } from "./components/AuthForm";
+import Home from "./components/Home";
+import { me } from "./store";
+import { loadFact } from "./store";
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      answer: 0,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.updateCampusThuck({ ...this.state, id: this.props.id });
+  }
+
   componentDidMount() {
-    this.props.loadInitialData()
+    this.props.loadFactThuck();
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log(prevProps);
+    if (this.props.fact.split(" ")[0] != this.state.answer) {
+      this.state.answer = this.props.fact.split(" ")[0];
+      console.log(this.state);
+    }
   }
 
   render() {
-    const {isLoggedIn} = this.props
-
     return (
-      <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Redirect to="/home" />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path='/' exact component={ Login } />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-          </Switch>
-        )}
-      </div>
-    )
+      <>
+        <div>
+          ___ {this.props.fact.substr(this.props.fact.indexOf(" ") + 1)}
+        </div>
+        {<button onClick={() => this.props.loadFactThuck()}>New fact</button>}
+      </>
+    );
   }
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (reduxState) => {
   return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id
-  }
-}
+    fact: reduxState.fact,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
-    loadInitialData() {
-      dispatch(me())
-    }
-  }
-}
+    loadFactThuck: () => dispatch(loadFact()),
+  };
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(connect(mapState, mapDispatch)(Routes));
---
+++ client/store/facts.js
@@ -0,0 +1,45 @@
+import jquery from "jquery";
+
+/**
+ * ACTION TYPES
+ */
+const GET_FACT = "GET_FACT";
+
+/**
+ * ACTION CREATORS
+ */
+const setFact = (fact) => ({ type: GET_FACT, fact });
+
+/**
+ * THUNK CREATORS
+ */
+
+export const loadFact = () => async (dispatch) => {
+  try {
+    let num = Math.floor(Math.random() * 100);
+    const triviaFact = await jquery.getJSON(
+      `http://numbersapi.com/${num}/trivia`,
+      function (data) {
+        console.log(data);
+      }
+    );
+
+    console.log(triviaFact.promise.responseText);
+    return dispatch(setFact(triviaFact.promise.responseText));
+  } catch (err) {
+    const triviaFact = err;
+    return dispatch(setFact(triviaFact.responseText));
+  }
+};
+
+/**
+ * REDUCER
+ */
+export default function (state = "no fact here!", action) {
+  switch (action.type) {
+    case GET_FACT:
+      return action.fact;
+    default:
+      return state;
+  }
+}
